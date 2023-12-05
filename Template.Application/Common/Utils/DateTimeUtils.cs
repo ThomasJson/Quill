b66e947e03896201493abaed5f15680afa0e817d
ThomasJson <joogletom8@gmail.com>
2023-12-05 11:26:36 +0100

    Changes

---
+++ Template.Application/Common/Utils/DateTimeUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Template.Application.Common.Utils
+{
+    public static class DateTimeUtils
+    {
+        public static DateTime ShortToSeconds(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Ticks - (dateTime.Ticks % TimeSpan.TicksPerSecond), dateTime.Kind);
+        }
+    }
+}
